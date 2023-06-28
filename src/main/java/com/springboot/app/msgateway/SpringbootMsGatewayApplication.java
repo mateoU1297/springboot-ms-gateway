148a35c109d3469b2b468300ee2aac57ed6e1557
mateoU1297 <castro.mateo@correounivalle.edu.co>
2023-06-28 16:45:02 -0500

    se crea repositorio

---
+++ src/main/java/com/springboot/app/msgateway/SpringbootMsGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.app.msgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMsGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/app/msgateway/SpringbootMsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.app.msgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMsGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
