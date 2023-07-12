fca38e3af9d15e936bf95b2bef56c42f2e8d0bd8
mateoU1297 <castro.mateo@correounivalle.edu.co>
2023-07-12 12:29:36 -0500

    se cambia de versión de spring y dependencias para que funcione correctamente, se agrega filtro para permitir o denegar accceso a las rutas

---
+++ src/main/java/com/springboot/app/msgateway/security/SpringSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.springboot.app.msgateway.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class SpringSecurityConfig {
+
+	@Bean
+	public SecurityWebFilterChain configure(ServerHttpSecurity http) {
+		return http.authorizeExchange().pathMatchers("/api/security/oauth/**").permitAll()
+				.pathMatchers(HttpMethod.GET, "/api/productos/listar", "/api/items/listar", "/api/usuarios/usuarios",
+						"/api/items/ver/{id}/cantidad/{cantidad}", "/api/productos/ver/{id}")
+				.permitAll().pathMatchers(HttpMethod.GET, "/api/usuarios/usuarios/{id}").hasAnyRole("ADMIN", "USER")
+				.pathMatchers("/api/productos/**", "/api/items/**", "/api/usuarios/usuarios/**").hasRole("ADMIN")
+				.anyExchange().authenticated().and().csrf().disable().build();
+	}
+
+}
